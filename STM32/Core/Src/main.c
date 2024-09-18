0d9f1d8798f983a63c9aab5f2702029e0034ad13
khangnguyen2k4 <khang.nguyen2k4@hcmut.edu.vn>
2024-09-18 20:06:12 +0700

    update ex6

--- STM32/Core/Src/main.c
+++ STM32/Core/Src/main.c
@@ -61,6 +61,14 @@ static void MX_GPIO_Init(void);
   * @brief  The application entry point.
   * @retval int
   */
+uint16_t ledPin[12] = { GPIO_PIN_4, GPIO_PIN_5, GPIO_PIN_6, GPIO_PIN_7, GPIO_PIN_8, GPIO_PIN_9, GPIO_PIN_10,
+						GPIO_PIN_11, GPIO_PIN_12, GPIO_PIN_13, GPIO_PIN_14, GPIO_PIN_15};
+void test(int n)
+{
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4| GPIO_PIN_5| GPIO_PIN_6| GPIO_PIN_7| GPIO_PIN_8| GPIO_PIN_9| GPIO_PIN_10|
+						GPIO_PIN_11| GPIO_PIN_12| GPIO_PIN_13| GPIO_PIN_14| GPIO_PIN_15, SET);
+	HAL_GPIO_WritePin(GPIOA, ledPin[n], RESET);
+}
 int main(void)
 {
   /* USER CODE BEGIN 1 */
@@ -91,9 +99,17 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+
+int n = 0;
   while (1)
   {
-
+	  test(n);
+	  n++;
+		if (n>=12)
+		{
+			n = 0;
+		}
+	  HAL_Delay(1000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
